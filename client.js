086d06ce5b81044dea02514944690cd9ae9797f7
Una Ho <Una_Ho@ose.com.tw>
2024-07-22 13:50:19 +0800

    try

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ TrelloPowerUp.initialize({
       return [
         {
           icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
-          text: "Una",
+          text: "Una2",
           callback: function (t) {
             return t.card('id') .then (function(card) {
                 return t.popup({
