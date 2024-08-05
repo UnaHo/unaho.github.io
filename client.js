798829a3c2b93378d85eb8349f1608c7baec2018
Una Ho <Una_Ho@ose.com.tw>
2024-08-05 16:00:42 +0800

    add loginMemberId

--- client.js
+++ client.js
@@ -3,15 +3,17 @@ TrelloPowerUp.initialize({
       return [
         {
           icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
-          text: "Una240722-8",
+          text: "Una240805-1",
           callback: function (t) {
             return t.card('id') .then (function(card) {
-              console.log(card)
+              //login member when press button
+              return t.member('id') .then (function(member) {
                 return t.popup({
                   title: 'GetCard',
-                  url: `estimate.html?id=${card.id}`
-                 });
-               });
+                  url: `estimate.html?id=${card.id}&member=${member.id}`
+                });
+              });
+            });
             
           },
         },
