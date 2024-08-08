ac04b3b88e4ada7a31568c63638644f7f0c09341
Una Ho <Una_Ho@ose.com.tw>
2024-08-08 17:48:15 +0800

    add SyncToPM

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ TrelloPowerUp.initialize({
       return [
         {
           icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
-          text: "Una240806-3",
+          text: "Una240808-1",
           callback: function (t) {
             return t.card('id') .then (function(card) {
               //login member when press button
